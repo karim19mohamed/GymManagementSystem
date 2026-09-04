f8d1a8027ff77e9a003ecca48f91620fccb76269
karim19mohamed <s.karim.ibrahim@gmail.com>
2026-09-04 04:43:28 +0300

    make Plans Controller

---
+++ GymManagementSystem.PL/Controllers/PlansController.cs
@@ -0,0 +1,20 @@
+﻿using GymManagementSystem.DAL.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.PL.Controllers {
+    public class PlansController(IPlanRepository planRepository) : Controller {
+        public async Task<IActionResult> Index() {
+            var plans = await planRepository.GetAllPlansAsync();
+            return View(plans);
+        }
+
+        public async Task<IActionResult> Details(int id) {
+            var plan = await planRepository.GetByIdAsync(id);
+            if (plan == null) {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+    }
+}
