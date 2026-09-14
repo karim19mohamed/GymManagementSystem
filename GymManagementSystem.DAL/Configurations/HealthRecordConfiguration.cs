0221c9f0b6659fec0f739a38063857a10e4480ee
karim19mohamed <s.karim.ibrahim@gmail.com>
2026-09-14 18:08:56 +0300

    add healthrecord configuration

---
+++ GymManagementSystem.DAL/Configurations/HealthRecordConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using GymManagementSystem.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.DAL.Configurations {
+    public class HealthRecordConfiguration : IEntityTypeConfiguration<HealthRecord> {
+        public void Configure(EntityTypeBuilder<HealthRecord> builder) {
+            builder.Property(h => h.BloodType)
+                   .HasMaxLength(5);
+            builder.Property(h => h.Note)
+                   .HasMaxLength(500);
+
+        }
+    }
+}
